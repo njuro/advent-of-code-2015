438a0fe541be85b33cc852da4f142f974195e08c
njuro <juraj.noge@gmail.com>
2020-12-25 18:28:03 +0100

    Day 24

---
+++ src/main/kotlin/day24.kt
@@ -0,0 +1,35 @@
+import utils.readInputLines
+import kotlin.math.min
+
+/** [https://adventofcode.com/2015/day/24] */
+class Day24 : AdventOfCodeTask {
+    private val packages = readInputLines("24.txt").map(String::toInt).sortedDescending()
+    private var smallestGroupSize = Int.MAX_VALUE
+
+    override fun run(part2: Boolean): Any {
+        val weightPerGroup = packages.sum() / if (part2) 4 else 3
+        return packages.flatMap { findCombinations(mutableSetOf(it), weightPerGroup) }
+            .filter { it.isNotEmpty() }.map { it.product() }.minOrNull()!!
+    }
+
+    private fun findCombinations(group: Set<Int>, weightPerGroup: Int): Set<Set<Int>> {
+        if (group.size > smallestGroupSize || group.sum() > weightPerGroup) {
+            return setOf(emptySet())
+        }
+
+        if (group.sum() == weightPerGroup) {
+            smallestGroupSize = min(smallestGroupSize, group.size)
+            return setOf(group)
+        }
+
+        return packages.filter { it < group.last() }.flatMap {
+            findCombinations(group + it, weightPerGroup)
+        }.toSet()
+    }
+
+    private fun Set<Int>.product() = fold(1L) { a, b -> a * b }
+}
+
+fun main() {
+    println(Day24().run(part2 = false))
+}

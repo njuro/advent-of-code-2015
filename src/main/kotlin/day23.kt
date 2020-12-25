d7c4c9b1d7f3b115c357270d05f5e9d042ff945d
njuro <juraj.noge@gmail.com>
2020-12-25 14:01:50 +0100

    Day 23

---
+++ src/main/kotlin/day23.kt
@@ -0,0 +1,40 @@
+import utils.readInputLines
+
+/** [https://adventofcode.com/2015/day/23] */
+class Day23 : AdventOfCodeTask {
+    override fun run(part2: Boolean): Any {
+        val instructions = readInputLines("23.txt").map {
+            val (instruction, arguments) = it.split(" ", limit = 2)
+            instruction to arguments.split(", ")
+        }
+        val registers = mutableMapOf("a" to if (part2) 1L else 0L, "b" to 0L)
+        var pointer = 0
+        
+        while (pointer in instructions.indices) {
+            val (instruction, arguments) = instructions[pointer]
+            when (instruction) {
+                "hlf" -> {
+                    registers[arguments[0]] = registers[arguments[0]]!! / 2
+                    pointer++
+                }
+                "tpl" -> {
+                    registers[arguments[0]] = registers[arguments[0]]!! * 3
+                    pointer++
+                }
+                "inc" -> {
+                    registers[arguments[0]] = registers[arguments[0]]!! + 1
+                    pointer++
+                }
+                "jmp" -> pointer += arguments[0].toInt()
+                "jie" -> if (registers[arguments[0]]!! % 2 == 0L) pointer += arguments[1].toInt() else pointer++
+                "jio" -> if (registers[arguments[0]]!! == 1L) pointer += arguments[1].toInt() else pointer++
+            }
+        }
+
+        return registers["b"]!!
+    }
+}
+
+fun main() {
+    println(Day23().run(part2 = true))
+}
